ef100a636e105ad32db698738ee2930cc9fd3625
Aron Ilie <aronilie.code@gmail.com>
2023-10-18 13:53:02 +0200

    Updated usePokemons test

--- src/hooks/usePokemons.test.js
+++ src/hooks/usePokemons.test.js
@@ -1,40 +1,66 @@
+import { PokemonsConsumer, PokemonsProvider } from "../store/pokemonsContext";
 import usePokemons from "./usePokemons";
+import { renderHook, act, waitFor } from "@testing-library/react";
 
 describe("Given a usePokemons hook", () => {
   describe("When is called the getPokemons function", () => {
-    it("Then it should return an array of pokemons on successful API call", async () => {
+    it("Then it should return an array of pokemons on a successful API call", async () => {
       global.fetch = jest.fn().mockResolvedValue({
         ok: true,
         json: () =>
           Promise.resolve({ pokemon_species: ["Pikachu", "Charmander"] }),
       });
+
       const expectedPokemons = ["Pikachu", "Charmander"];
 
-      const pokemonService = usePokemons();
-      const pokemons = await pokemonService.getPokemons();
+      const { result: pokemonsStore } = renderHook(() => PokemonsConsumer(), {
+        wrapper: PokemonsProvider,
+      });
 
-      expect(pokemons).toEqual(expectedPokemons);
+      const { result } = renderHook(() => usePokemons(), {
+        wrapper: PokemonsProvider,
+      });
+
+      await act(async () => {
+        await result.current.getPokemons();
+      });
+
+      setTimeout(() => {
+        const pokemons = pokemonsStore.current.pokemons;
+
+        expect(pokemons).toEqual(expectedPokemons);
+      }, 0);
 
       expect(fetch).toHaveBeenCalledWith(
         "https://pokeapi.co/api/v2/generation/1"
       );
     });
-  });
 
-  it("should throw an error on failed API call", async () => {
-    global.fetch = jest.fn().mockResolvedValue({
-      ok: false,
-      status: 404,
+    describe("And the api call is wrong", () => {
+      it("Then it should throw an error on failed API call", async () => {
+        global.fetch = jest.fn().mockResolvedValue({
+          ok: false,
+          status: 404,
+        });
+
+        const expectedError = new Error("Error-HTTP: 404");
+
+        const { result } = renderHook(() => usePokemons(), {
+          wrapper: PokemonsProvider,
+        });
+
+        try {
+          await act(async () => {
+            await result.current.getPokemons();
+          });
+        } catch (error) {
+          expect(error).toEqual(expectedError);
+        }
+
+        expect(fetch).toHaveBeenCalledWith(
+          "https://pokeapi.co/api/v2/generation/1"
+        );
+      });
     });
-
-    const pokemonService = usePokemons();
-
-    await expect(pokemonService.getPokemons()).rejects.toThrow(
-      "Error-HTTP: 404"
-    );
-
-    expect(fetch).toHaveBeenCalledWith(
-      "https://pokeapi.co/api/v2/generation/1"
-    );
   });
 });
---
+++ src/utils/testUtils.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { PokemonsProvider } from "../store/pokemonsContext";
+
+const AllProviders = ({ children }) => {
+  return <PokemonsProvider>{children}</PokemonsProvider>;
+};
+
+const customRender = (ui, options) =>
+  render(ui, { wrapper: AllProviders, ...options });
+
+export * from "@testing-library/react";
+
+export { customRender as render };
