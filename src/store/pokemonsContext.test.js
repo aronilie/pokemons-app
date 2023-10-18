7ea9f1ed7b362f6cacacfff7254f2b492a7c0472
Aron Ilie <aronilie.code@gmail.com>
2023-10-18 14:25:53 +0200

    Tested restorePokemons function from pokemonsContext

--- src/store/pokemonsContext.test.js
+++ src/store/pokemonsContext.test.js
@@ -1,4 +1,4 @@
-import { renderHook, act } from "@testing-library/react";
+import { renderHook, act, waitFor } from "@testing-library/react";
 import { PokemonsConsumer, PokemonsProvider } from "./pokemonsContext";
 
 describe("Given a pokemonsContext context", () => {
@@ -17,6 +17,32 @@ describe("Given a pokemonsContext context", () => {
       const updatedPokemons = pokemonsStore.current.pokemons;
 
       expect(updatedPokemons).toEqual(pokemonsToUpdate);
+    });
+  });
+
+  describe("When restorePokemons is called", () => {
+    test("Then it should restore all the existent pokemons to none (empty array)", async () => {
+      const pokemonsToUpdate = ["Pikachu", "Charmander"];
+
+      const { result: pokemonsStore } = renderHook(() => PokemonsConsumer(), {
+        wrapper: PokemonsProvider,
+      });
+
+      await act(async () => {
+        await pokemonsStore.current.updatePokemons(pokemonsToUpdate);
+      });
+
+      let updatedPokemons = pokemonsStore.current.pokemons;
+
+      expect(updatedPokemons).toEqual(pokemonsToUpdate);
+
+      await act(async () => {
+        await pokemonsStore.current.restorePokemons();
+      });
+
+      waitFor(() => {
+        expect(updatedPokemons).toEqual([]);
+      });
     });
   });
 });
