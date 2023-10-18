ccb0d0dcea992ff2712bc1d2414e02ecda44f421
Aron Ilie <aronilie.code@gmail.com>
2023-10-18 14:24:09 +0200

    Tested updatePokemons function from pokemonsContext

---
+++ src/store/pokemonsContext.test.js
@@ -0,0 +1,22 @@
+import { renderHook, act } from "@testing-library/react";
+import { PokemonsConsumer, PokemonsProvider } from "./pokemonsContext";
+
+describe("Given a pokemonsContext context", () => {
+  describe("When updatePokemons is called with new pokemons", () => {
+    test("Then it should update the pokemons store", async () => {
+      const pokemonsToUpdate = ["Pikachu", "Charmander"];
+
+      const { result: pokemonsStore } = renderHook(() => PokemonsConsumer(), {
+        wrapper: PokemonsProvider,
+      });
+
+      await act(async () => {
+        await pokemonsStore.current.updatePokemons(pokemonsToUpdate);
+      });
+
+      const updatedPokemons = pokemonsStore.current.pokemons;
+
+      expect(updatedPokemons).toEqual(pokemonsToUpdate);
+    });
+  });
+});
